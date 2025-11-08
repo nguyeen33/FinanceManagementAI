5fac4273e3c03aab8d5426de4b0dbac05d8a0bcc
nguyeeen <nvnguyen0304@gmail.com>
2025-11-08 14:47:39 +0700

    Complete Nav + Footer + About + Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-export default function HomePage() {
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "@/components/Guest";
+
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest/>;
+  }
+
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the application.</p>
     </main>
   );
-}
+} 
