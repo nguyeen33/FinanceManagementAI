b6b8c7cae25371e99001fa459e7ad24cebab3b47
nguyeeen <nvnguyen0304@gmail.com>
2025-11-08 17:12:28 +0700

    ....

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ClerkThemeProvider from '@/components/ClerkThemeProvider';
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/Navbar';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 
