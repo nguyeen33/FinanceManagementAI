9bed467dbf88a957efba1933c37fab49027e0bcb
nguyeeen <nvnguyen0304@gmail.com>
2025-11-08 17:21:26 +0700

    Fix case sensitivity for Navbar file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ClerkThemeProvider from '@/components/ClerkThemeProvider';
-import Navbar from '../components/Navbar';
 import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+
 import { ThemeProvider } from '@/contexts/ThemeContext';
 
 const geistSans = Geist({
